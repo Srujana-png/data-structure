87ad3ae4a5d16fcf0fdf8b2663a04ffe5a83d73c
Srujana-png <vijayaramanathi@gmail.com>
2026-02-13 11:03:57 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct student
+{
+	int roll;
+	char name[20];
+	float marks;
+	char grade;
+}; 
+int main()
+{
+	struct student s;
+    printf("\n enter student roll number:");
+	scanf("%d",&s.roll);
+	printf("\n enter student name:");
+	scanf("%s",&s.name);
+	printf("\n enter student marks:");
+	scanf("%f",&s.marks);
+	printf("\n enter student grade:");
+	scanf("%c",&s.grade);
+	printf("\nstudent roll number:%d\nstudent name:%s\nstudent marks:%f\nstudent grade:%c",s.roll,s.name,s.marks,s.grade);
+	return 0;
+}
