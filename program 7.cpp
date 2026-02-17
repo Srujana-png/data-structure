307419e9bee4f57ada6420f8486102d6d8c0bbe9
Srujana-png <vijayaramanathi@gmail.com>
2026-02-17 19:32:49 +0530

    Add files via upload

---
+++ program 7.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void display(int a, float b);
+struct  student
+{
+	int roll;
+	float marks;
+};
+int main()
+{
+	struct student s;
+	printf("\nenter student roll number:");
+	scanf("%d",&s.roll);
+	printf("\nenter student marks:");
+	scanf("%f",&s.marks);
+	display(s.roll,s.marks);
+	return 0;
+}
+void display(int a, float b)
+{
+	printf("\nstudent roll number:%d\nstudent marks: %f",a,b);
+}
